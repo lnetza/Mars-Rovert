a7cabb642712020b6d74a1efde417dff290eae45
lnetza <l.lopeznetza@gmail.com>
2020-08-31 18:10:53 -0500

    state formulario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React,{Fragment} from 'react';
+import React,{Fragment,useState} from 'react';
 import Formulario from './Components/Formulario'
 
 function App() {
+
+  const [busqueda, guardarBusqueda] = useState({
+    sol:'',
+    fecha:'',
+    camara:''
+  });
+
   return (
     <Fragment>
       <h1>Mars</h1>
-      <Formulario/>
+      <div className="row">
+          <div className="col m6 push-m1">
+            <Formulario
+            />
+          </div>
+
+           <div className="col m6 ">
+            <h2>Resultado</h2>
+          </div>
+
+      </div>
     </Fragment>
   );
 }
--- src/Components/Formulario.js
+++ src/Components/Formulario.js
@@ -1,50 +1,54 @@
 import React from 'react';
 
-const Formulario = () => {
+const Formulario = ({busqueda,guardarBusqueda}) => {
+
+    const {sol,camara, fecha}=busqueda;
+
     return ( 
+        
         <div className="row"> 
            <form>   
                     <div className="row">
-                    <div className="input-field col s3">
-                        <input  id="sol" type="text"/>
-                        <label htmlFor="sol">Sol Marciano:</label>
-                    </div>
-                    </div>
-
-                    <div className="row">
-                    <div className="input-field col s3">
-                        <select>
-                        <option value="">Selecciona una camara</option>
-                        <option value="fhaz">FHAZ</option>
-                        <option value="rhaz">RHAZ</option>
-                        <option value="mast">MAST</option>
-                        <option value="chemcam">CHEMCAM</option>
-                        <option value="mahli">MAHLI</option>
-                        <option value="mardi">MARDI</option>
-                        <option value="navcam">NAVCAM</option>
-                        <option value="pancam">PANCAM</option>
-                        <option value="minities">MINITES</option>
-                        </select>
-                        <label>Camara</label>
-                    </div>
+                        <div className="input-field col s3">
+                            <input  id="sol" type="text" value={sol}/>
+                            <label htmlFor="sol">Sol Marciano:</label>
+                        </div>
                     </div>
 
                     <div className="row">
-                    <div className="input-field col s3">
-                        <input  id="fecha" type="date"/>
-                        <label htmlFor="fecha">Fecha:</label>
+                        <div className="input-field col s3">
+                            <select name="camara" id="camara" value={camara}>
+                            <option value="">Selecciona una camara</option>
+                            <option value="fhaz">FHAZ</option>
+                            <option value="rhaz">RHAZ</option>
+                            <option value="mast">MAST</option>
+                            <option value="chemcam">CHEMCAM</option>
+                            <option value="mahli">MAHLI</option>
+                            <option value="mardi">MARDI</option>
+                            <option value="navcam">NAVCAM</option>
+                            <option value="pancam">PANCAM</option>
+                            <option value="minities">MINITES</option>
+                            </select>
+                            <label>Camara</label>
+                        </div>
                     </div>
-                    </div>
+
                     <div className="row">
-                    <div className="col s3">
-                    <button class="btn waves-effect waves-light" type="submit">Buscar
-                        
-                    </button>
+                        <div className="input-field col s3">
+                            <input  id="fecha" type="date" value={fecha}/>
+                            <label htmlFor="fecha">Fecha:</label>
+                        </div>
                     </div>
+
+                    <div className="row">
+                        <div className="col s3">
+                            <button className="btn waves-effect waves-light" type="submit">
+                                Buscar   
+                            </button>
+                        </div>
                     </div>
            </form>
         </div>
-      
      );
 }
  
