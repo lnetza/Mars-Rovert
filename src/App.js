17202d746eacfb4cbdb645a0784973ea2149612c
lnetza <l.lopeznetza@gmail.com>
2020-09-01 00:25:10 -0500

    componente data y app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React,{Fragment,useState} from 'react';
-import Formulario from './Components/Formulario'
+import React,{Fragment,useState,useEffect} from 'react';
+import Formulario from './Components/Formulario';
+import Data from './Components/Data';
 
 function App() {
 
@@ -9,17 +10,57 @@ function App() {
     camara:''
   });
 
+  const {sol, fecha, camara} = busqueda;
+
+  const [consultar, guardarConsultar] =useState(false);
+  const [resultado, guardarResultado] = useState({});
+
+  useEffect(() => {
+
+    const consultarAPI = async()=>{
+      if (consultar){
+        const apiID='Zd0CREJIccbZtbJNBaeDm0wey9EAgharcDnOM0cL';
+        const URL=`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&camera=${camara}&earth_date=${fecha}&api_key=${apiID}`;
+
+        const respuesta = await fetch(URL);
+        const resultado = await respuesta.json();
+        
+        /*
+        const arrayresultado =[];
+        
+        for(let i=0; i<resultado.photos.length;i++){
+          arrayresultado.push(resultado.photos[i].img_src);
+          
+      }*/
+
+        guardarResultado(resultado);
+      }
+    }
+    consultarAPI();
+
+  }, [consultar]);
+
+
+
   return (
     <Fragment>
       <h1>Mars</h1>
       <div className="row">
           <div className="col m6 push-m1">
             <Formulario
+              busqueda={busqueda}
+              consultar={consultar}
+              guardarBusqueda={guardarBusqueda}
+              guardarConsultar={guardarConsultar}
+              
             />
           </div>
 
            <div className="col m6 ">
             <h2>Resultado</h2>
+            <Data
+              resultado={resultado}
+            />
           </div>
 
       </div>
--- src/Components/Formulario.js
+++ src/Components/Formulario.js
@@ -1,33 +1,63 @@
 import React from 'react';
 
-const Formulario = ({busqueda,guardarBusqueda}) => {
+const Formulario = ({busqueda,guardarBusqueda,consultar,guardarConsultar}) => {
 
+    
     const {sol,camara, fecha}=busqueda;
+
+    //Captura el cambio de valores en los campos
+    const handleChange= event=>{
+        //Actualizar el state
+        guardarBusqueda({
+            ...busqueda,
+            [event.target.name]:event.target.value
+        })
+    }
+
+    //Cuando el usuario da submit 
+    const handleSubmmit = e =>{
+        e.preventDefault();
+
+        guardarConsultar(true);
+    }
 
     return ( 
         
         <div className="row"> 
-           <form>   
+           <form
+                onSubmit={handleSubmmit}
+           >   
                     <div className="row">
                         <div className="input-field col s3">
-                            <input  id="sol" type="text" value={sol}/>
+                            <input  
+                                name="sol"
+                                id="sol" 
+                                type="text" 
+                                value={sol}
+                                onChange={handleChange}
+                            />
                             <label htmlFor="sol">Sol Marciano:</label>
                         </div>
                     </div>
 
                     <div className="row">
-                        <div className="input-field col s3">
-                            <select name="camara" id="camara" value={camara}>
-                            <option value="">Selecciona una camara</option>
-                            <option value="fhaz">FHAZ</option>
-                            <option value="rhaz">RHAZ</option>
-                            <option value="mast">MAST</option>
-                            <option value="chemcam">CHEMCAM</option>
-                            <option value="mahli">MAHLI</option>
-                            <option value="mardi">MARDI</option>
-                            <option value="navcam">NAVCAM</option>
-                            <option value="pancam">PANCAM</option>
-                            <option value="minities">MINITES</option>
+                        <div className="input-field col s5">
+                            <select 
+                                name="camara" 
+                                id="camara" 
+                                value={camara}
+                                onChange={handleChange}
+                            >
+                                <option value="">Selecciona una camara</option>
+                                <option value="fhaz">Cámara delantera para evitar peligros</option>
+                                <option value="rhaz">Cámara trasera para evitar peligros</option>
+                                <option value="mast">Cámara de mástil</option>
+                                <option value="chemcam">Complejo de cámara y química</option>
+                                <option value="mahli">Lector de imágenes Mars Hand Lens</option>
+                                <option value="mardi">Generador de imágenes Mars Descent</option>
+                                <option value="navcam">Cámara de navegación</option>
+                                <option value="pancam">Cámara panorámica</option>
+                                <option value="minities">Espectrómetro de emisión térmica en miniatura (Mini-TES)</option>
                             </select>
                             <label>Camara</label>
                         </div>
@@ -35,7 +65,14 @@ const Formulario = ({busqueda,guardarBusqueda}) => {
 
                     <div className="row">
                         <div className="input-field col s3">
-                            <input  id="fecha" type="date" value={fecha}/>
+                            <input  
+                                placeholder=""
+                                name="fecha"
+                                id="fecha" 
+                                type="date" 
+                                value={fecha}
+                                onChange={handleChange}
+                            />
                             <label htmlFor="fecha">Fecha:</label>
                         </div>
                     </div>
@@ -48,6 +85,7 @@ const Formulario = ({busqueda,guardarBusqueda}) => {
                         </div>
                     </div>
            </form>
+    
         </div>
      );
 }
