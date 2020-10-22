d2779a4fb562f1efc2a4d2ee5db84026b650b8de
lnetza <l.lopeznetza@gmail.com>
2020-10-22 00:58:09 -0500

    prettier

--- src/Components/Fotos.js
+++ src/Components/Fotos.js
@@ -1,22 +1,19 @@
-import React from 'react';
+import React from "react";
 
-const Fotos = ({informacion}) => {
-    //extraer la data
-    const {sol,camera,img_src,earth_date,rover}=informacion;
+const Fotos = ({ informacion }) => {
+  //extraer la data
+  const { sol, camera, img_src, earth_date, rover } = informacion;
 
-    return ( 
+  return (
+    <div className="galeria">
+      <div className="col s3 m3">
+        <img className="responsive-img materialboxed" src={img_src} />
+        <span className="card-title">{camera.full_name}</span>
+        <p>Sol Marciano:{sol}</p>
+        <span className="card-title">Fecha en tierra: {earth_date}</span>
+      </div>
+    </div>
+  );
+};
 
-        <div className="galeria">
-                <div className="col s3 m3">
-                    <img className="responsive-img materialboxed" src={img_src} />
-                    <span className="card-title">{camera.full_name}</span>
-                    <p>Sol Marciano:{sol}</p>
-                    <span className="card-title">Fecha en tierra: {earth_date}</span>
-
-                </div>
-        </div>
-     );
-}
- 
 export default Fotos;
-
