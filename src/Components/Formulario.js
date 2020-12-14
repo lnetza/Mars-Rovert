bbd96c72f33b2d528a0245bfa7d145749c31506c
lnetza <l.lopeznetza@gmail.com>
2020-12-14 13:26:27 -0600

    Form, Mars, Fotos

--- src/Components/Formulario.js
+++ src/Components/Formulario.js
@@ -1,136 +1,132 @@
-import React, { useState } from "react";
-import Error from "./Error";
+import React,{useState} from 'react';
+import Error from './Error';
 
-const Formulario = ({
-  busqueda,
-  guardarBusqueda,
-  guardarConsultar,
-  guardarCaso,
-}) => {
-  const { sol, camara, fecha } = busqueda;
+const Formulario = ({busqueda,guardarBusqueda,guardarConsultar,guardarCaso}) => {
 
-  const [errorFormulario, guardarErrorFormulario] = useState(false);
+    
+    const {sol,camara, fecha}=busqueda;
 
-  //Captura el cambio de valores en los campos
-  const handleChange = (event) => {
-    //Actualizar el state
-    guardarBusqueda({
-      ...busqueda,
-      [event.target.name]: event.target.value,
-    });
-  };
+    
+    const [errorFormulario, guardarErrorFormulario]=useState(false);
 
-  //Cuando el usuario da submit
-  const handleSubmmit = (e) => {
-    e.preventDefault();
-
-    //validar
-    if (fecha !== "" && camara !== "" && sol !== "") {
-      guardarCaso("all");
-      guardarConsultar(true);
-    } else if (sol !== "" && camara.trim() === "" && fecha === "") {
-      guardarCaso("sol");
-      guardarConsultar(true);
-      return;
-    } else if (fecha !== "" && camara.trim() === "" && sol === "") {
-      guardarCaso("fecha");
-      guardarConsultar(true);
-      return;
-    } else if (sol.trim() === "" && fecha.trim() === "" && camara !== "") {
-      guardarCaso("soloCamara");
-      guardarConsultar(true);
-      return;
-    } else if (sol !== "" && fecha !== "" && camara.trim() === "") {
-      guardarCaso("solYFecha");
-      guardarConsultar(true);
-      return;
-    } else if (sol !== "" && camara !== "" && fecha.trim() === "") {
-      guardarCaso("solYCamara");
-      guardarConsultar(true);
-      return;
-    } else if (camara !== "" && fecha !== "" && sol.trim() === "") {
-      guardarCaso("camaraYFecha");
-      guardarConsultar(true);
-      return;
-    } else if (
-      camara.trim() === "" &&
-      fecha.trim() === "" &&
-      sol.trim() === ""
-    ) {
-      guardarErrorFormulario(true);
-      return;
+    //Captura el cambio de valores en los campos
+    const handleChange= event=>{
+        //Actualizar el state
+        guardarBusqueda({
+            ...busqueda,
+            [event.target.name]:event.target.value
+        })
     }
-  };
 
-  return (
-    <div className="row">
-      <form onSubmit={handleSubmmit}>
-        {" "}
-        {errorFormulario ? (
-          <Error mensaje="Selecciona al menos un campo" />
-        ) : null}
-        <div className="row">
-          <div className="input-field col s3">
-            <input
-              name="sol"
-              id="sol"
-              type="text"
-              value={sol}
-              onChange={handleChange}
-              placeholder="Ejemplo: 223"
-            />
-            <label htmlFor="sol">Sol Marciano:</label>
-          </div>
-        </div>
-        <div className="row">
-          <div className="input-field col s5">
-            <select
-              name="camara"
-              id="camara"
-              value={camara}
-              onChange={handleChange}
-            >
-              <option value="">Selecciona una camara</option>
-              <option value="fhaz">
-                Cámara delantera para evitar peligros
-              </option>
-              <option value="rhaz">Cámara trasera para evitar peligros</option>
-              <option value="mast">Cámara de mástil</option>
-              <option value="chemcam">Complejo de cámara y química</option>
-              <option value="mahli">Lector de imágenes Mars Hand Lens</option>
-              <option value="mardi">Generador de imágenes Mars Descent</option>
-              <option value="navcam">Cámara de navegación</option>
-              <option value="pancam">Cámara panorámica</option>
-              <option value="minities">
-                Espectrómetro de emisión térmica en miniatura (Mini-TES)
-              </option>
-            </select>
-            <label>Camara</label>
-          </div>
-        </div>
-        <div className="row">
-          <div className="input-field col s3">
-            <input
-              placeholder=""
-              name="fecha"
-              id="fecha"
-              type="date"
-              value={fecha}
-              onChange={handleChange}
-            />
-            <label htmlFor="fecha">Fecha:</label>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col s3">
-            <button className="btn waves-effect waves-light" type="submit">
-              Buscar
-            </button>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
-};
+    //Cuando el usuario da submit 
+    const handleSubmmit = e =>{
+        e.preventDefault();
 
+        //validar
+         if (fecha !=='' && camara !=='' && sol !==''){
+            guardarCaso('all');
+            guardarConsultar(true);
+        }else if(sol !=='' && camara.trim() ==='' && fecha ===''){
+            guardarCaso('sol');
+            guardarConsultar(true);
+            return;
+        }else if(fecha !=='' && camara.trim() ==='' && sol ===''){
+            guardarCaso('fecha');
+            guardarConsultar(true);
+            return;
+        } else if (sol.trim()==='' && fecha.trim()==='' && camara !==''){
+            guardarCaso('soloCamara');
+            guardarConsultar(true);
+            return;
+        }else if (sol!=='' && fecha!=='' && camara.trim()===''){
+            guardarCaso('solYFecha');
+            guardarConsultar(true);
+            return;
+        }else if (sol!=='' && camara!=='' && fecha.trim() ===''){
+            guardarCaso('solYCamara');
+            guardarConsultar(true);
+            return;
+        }else if(camara!=='' && fecha!==''&& sol.trim()===''){
+            guardarCaso('camaraYFecha');
+            guardarConsultar(true);
+            return;
+        }else if(camara.trim()==='' && fecha.trim()===''&& sol.trim()===''){
+            guardarErrorFormulario(true);
+            return;
+        }
+
+
+        
+    }
+
+    return ( 
+        
+        <div className="row">
+           <form
+                onSubmit={handleSubmmit}
+           >        {(errorFormulario)? <Error mensaje="Selecciona al menos un campo"/>:null}
+                    <div className="row">
+                        <div className="input-field col s3">
+                            <input  
+                                name="sol"
+                                id="sol" 
+                                type="text" 
+                                value={sol}
+                                onChange={handleChange}
+                                placeholder="Ejemplo: 223"
+                            />
+                            <label htmlFor="sol">Sol Marciano:</label>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="input-field col s5">
+                            <select 
+                                name="camara" 
+                                id="camara" 
+                                value={camara}
+                                onChange={handleChange}
+                            >
+                                <option value="">Selecciona una camara</option>
+                                <option value="fhaz">Cámara delantera para evitar peligros</option>
+                                <option value="rhaz">Cámara trasera para evitar peligros</option>
+                                <option value="mast">Cámara de mástil</option>
+                                <option value="chemcam">Complejo de cámara y química</option>
+                                <option value="mahli">Lector de imágenes Mars Hand Lens</option>
+                                <option value="mardi">Generador de imágenes Mars Descent</option>
+                                <option value="navcam">Cámara de navegación</option>
+                                <option value="pancam">Cámara panorámica</option>
+                                <option value="minities">Espectrómetro de emisión térmica en miniatura (Mini-TES)</option>
+                            </select>
+                            <label>Camara</label>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="input-field col s3">
+                            <input  
+                                placeholder=""
+                                name="fecha"
+                                id="fecha" 
+                                type="date" 
+                                value={fecha}
+                                onChange={handleChange}
+                            />
+                            <label htmlFor="fecha">Fecha:</label>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col s3">
+                            <button className="btn waves-effect waves-light" type="submit">
+                                Buscar   
+                            </button>
+                        </div>
+                    </div>
+           </form>
+    
+        </div>
+     );
+}
+ 
 export default Formulario;
--- src/Components/Mars.js
+++ src/Components/Mars.js
@@ -1,29 +1,128 @@
-import React, { Fragment, useState, useEffect } from "react";
-import Formulario from "./Formulario";
-import ListarFotos from "./ListarFotos";
-import Error from "./Error";
+import React,{Fragment,useState,useEffect} from 'react';
+import Formulario from './Formulario';
+import ListarFotos from './ListarFotos';
+import Error from './Error';
 
 function Mars() {
+
+  
+
   const [busqueda, guardarBusqueda] = useState({
-    sol: "",
-    fecha: "",
-    camara: "",
+    sol:'',
+    fecha:'',
+    camara:''
   });
 
-  const { sol, fecha, camara } = busqueda;
+  const {sol, fecha, camara} = busqueda;
 
-  const [consultar, guardarConsultar] = useState(false);
+  const [consultar, guardarConsultar] =useState(false);
   const [resultado, guardarResultado] = useState([]);
-  const [caso, guardarCaso] = useState("");
-  const [errorAPI, guardarErrorAPI] = useState(false);
+  const [caso, guardarCaso] =useState('');
+  const [errorAPI, guardarErrorAPI]=useState(false);
 
+ 
+  
   useEffect(() => {
-    const consultarAPI = async () => {
-      const apiID = "Zd0CREJIccbZtbJNBaeDm0wey9EAgharcDnOM0cL";
 
-      if (consultar && caso === "sol") {
-        const URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&api_key=${apiID}`;
+    
 
+    const consultarAPI = async()=>{
+      const apiID='Zd0CREJIccbZtbJNBaeDm0wey9EAgharcDnOM0cL';
+
+      if (consultar && caso === 'sol'){
+        
+        const URL=`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&api_key=${apiID}`;  
+
+        //const URL=`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&camera=${camara}&earth_date=${fecha}&api_key=${apiID}`;
+
+        const respuesta = await fetch(URL);
+        const datosRespuesta = await respuesta.json();
+        
+        guardarResultado(datosRespuesta.photos);
+
+        guardarConsultar(false);
+
+        if(datosRespuesta.photos.length===0){
+          guardarErrorAPI(true);
+        }else{
+          guardarErrorAPI(false);
+        }
+      }
+      
+      if (consultar && caso === 'fecha'){
+        const URL=`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?&earth_date=${fecha}&api_key=${apiID}`;  
+
+        
+        const respuesta = await fetch(URL);
+        const datosRespuesta = await respuesta.json();
+        
+        guardarResultado(datosRespuesta.photos);
+
+        guardarConsultar(false);
+
+        if(datosRespuesta.photos.length===0){
+          guardarErrorAPI(true);
+        }else{
+          guardarErrorAPI(false);
+        }
+      }
+
+      if (consultar && caso === 'all'){
+        const URL=`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&camera=${camara}&earth_date=${fecha}&api_key=${apiID}`;
+
+        
+        const respuesta = await fetch(URL);
+        const datosRespuesta = await respuesta.json();
+        
+        guardarResultado(datosRespuesta.photos);
+
+        guardarConsultar(false);
+
+        if(datosRespuesta.photos.length===0){
+          guardarErrorAPI(true);
+        }else{
+          guardarErrorAPI(false);
+        }
+      }
+
+      if (consultar && caso === 'solYFecha'){
+        const URL=`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&earth_date=${fecha}&api_key=${apiID}`;
+
+        
+        const respuesta = await fetch(URL);
+        const datosRespuesta = await respuesta.json();
+        
+        guardarResultado(datosRespuesta.photos);
+
+        guardarConsultar(false);
+
+        if(datosRespuesta.photos.length===0){
+          guardarErrorAPI(true);
+        }else{
+          guardarErrorAPI(false);
+        }
+      }
+      if (consultar && caso === 'solYCamara'){
+        const URL=`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&camera=${camara}&api_key=${apiID}`;
+
+        
+        const respuesta = await fetch(URL);
+        const datosRespuesta = await respuesta.json();
+        
+        guardarResultado(datosRespuesta.photos);
+
+        guardarConsultar(false);
+
+        if(datosRespuesta.photos.length===0){
+          guardarErrorAPI(true);
+        }else{
+          guardarErrorAPI(false);
+        }
+      }
+      if (consultar && caso === 'camaraYFecha'){
+        const URL=`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?camera=${camara}&earth_date=${fecha}&api_key=${apiID}`;
+
+        
         const respuesta = await fetch(URL);
         const datosRespuesta = await respuesta.json();
 
@@ -31,128 +130,60 @@ function Mars() {
 
         guardarConsultar(false);
 
-        if (datosRespuesta.photos.length === 0) {
+        if(datosRespuesta.photos.length===0){
           guardarErrorAPI(true);
-        } else {
+        }else{
           guardarErrorAPI(false);
         }
       }
 
-      if (consultar && caso === "fecha") {
-        const URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?&earth_date=${fecha}&api_key=${apiID}`;
+      if (consultar && caso === 'soloCamara'){
 
-        const respuesta = await fetch(URL);
-        const datosRespuesta = await respuesta.json();
-
-        guardarResultado(datosRespuesta.photos);
-
-        guardarConsultar(false);
-
-        if (datosRespuesta.photos.length === 0) {
-          guardarErrorAPI(true);
-        } else {
-          guardarErrorAPI(false);
-        }
-      }
-
-      if (consultar && caso === "all") {
-        const URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&camera=${camara}&earth_date=${fecha}&api_key=${apiID}`;
-
-        const respuesta = await fetch(URL);
-        const datosRespuesta = await respuesta.json();
-
-        guardarResultado(datosRespuesta.photos);
-
-        guardarConsultar(false);
-
-        if (datosRespuesta.photos.length === 0) {
-          guardarErrorAPI(true);
-        } else {
-          guardarErrorAPI(false);
-        }
-      }
-
-      if (consultar && caso === "solYFecha") {
-        const URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&earth_date=${fecha}&api_key=${apiID}`;
-
-        const respuesta = await fetch(URL);
-        const datosRespuesta = await respuesta.json();
-
-        guardarResultado(datosRespuesta.photos);
-
-        guardarConsultar(false);
-
-        if (datosRespuesta.photos.length === 0) {
-          guardarErrorAPI(true);
-        } else {
-          guardarErrorAPI(false);
-        }
-      }
-      if (consultar && caso === "solYCamara") {
-        const URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&camera=${camara}&api_key=${apiID}`;
-
-        const respuesta = await fetch(URL);
-        const datosRespuesta = await respuesta.json();
-
-        guardarResultado(datosRespuesta.photos);
-
-        guardarConsultar(false);
-
-        if (datosRespuesta.photos.length === 0) {
-          guardarErrorAPI(true);
-        } else {
-          guardarErrorAPI(false);
-        }
-      }
-      if (consultar && caso === "camaraYFecha") {
-        const URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?camera=${camara}&earth_date=${fecha}&api_key=${apiID}`;
-
-        const respuesta = await fetch(URL);
-        const datosRespuesta = await respuesta.json();
-
-        guardarResultado(datosRespuesta.photos);
-
-        guardarConsultar(false);
-
-        if (datosRespuesta.photos.length === 0) {
-          guardarErrorAPI(true);
-        } else {
-          guardarErrorAPI(false);
-        }
-      }
-
-      if (consultar && caso === "soloCamara") {
         guardarConsultar(false);
         guardarErrorAPI(true);
       }
-    };
+
+    }
     consultarAPI();
+
   }, [consultar]);
 
+ 
+
+
+  
   //carga condicional de componente
   let componente;
-
-  if (errorAPI) {
-    componente = <Error mensaje="No se encontrarón datos" />;
-  } else {
-    componente = <ListarFotos resultado={resultado} />;
+  
+  if(errorAPI){
+    componente=<Error
+                mensaje="No se encontrarón datos"
+                />
+  }else{
+    componente=<ListarFotos
+                resultado={resultado}
+                />
   }
 
+
   return (
+    
     <Fragment>
       <div className="row">
-        <div className="col m6 push-m1">
-          <Formulario
-            busqueda={busqueda}
-            consultar={consultar}
-            caso={caso}
-            guardarBusqueda={guardarBusqueda}
-            guardarConsultar={guardarConsultar}
-            guardarCaso={guardarCaso}
-          />
-        </div>
+          <div className="col m6 push-m1">
+            <Formulario
+              busqueda={busqueda}
+              consultar={consultar}
+              caso={caso}
+              guardarBusqueda={guardarBusqueda}
+              guardarConsultar={guardarConsultar}
+              guardarCaso={guardarCaso}
+            />
+          </div>
 
-        <div className="col m6">{componente}</div>
+           <div className="col m6">
+              {componente}
+          </div>
       </div>
     </Fragment>
   );
