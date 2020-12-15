6d4b931141e8cb818c0a1303efc6eae9080a88e0
lnetza <l.lopeznetza@gmail.com>
2020-12-15 14:46:20 -0600

    se agrega archivo config y se modifica Mars component

---
+++ src/config.js
@@ -0,0 +1,12 @@
+//Configuracion para MARS-ROVERS
+
+//url all params example `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${sol}&camera=${camara}&earth_date=${fecha}&api_key=${apiID}`;
+
+const API_URL = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?';
+const API_KEY = 'Zd0CREJIccbZtbJNBaeDm0wey9EAgharcDnOM0cL';
+
+
+export{
+    API_KEY,
+    API_URL
+}
